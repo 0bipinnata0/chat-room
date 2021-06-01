3bc6ac5a375da5897f533946720bac19b51493ef
夕向 <odinzero@outlook.com>
2021-06-01 15:28:49 +0800

    init

---
+++ chat-room/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header>头部</header>
+      <main>主体</main>
+      <footer>结束</footer>
+    </div>
+  );
+}
+
+export default App;
